705c032aafd8823b2c99a84bd6cddb205808b578
Keeran Srikugan <keeran113@gmail.com>
2021-08-16 00:05:35 -0400

    first commit building a javafx and gradle file

---
+++ src/main/java/Controller.java
@@ -0,0 +1,37 @@
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controller {
+
+    public TextField username;
+    public TextField password;
+    public ListView files;
+    public Button copyButton;
+    public Button clearButton;
+    public Button deleteButton;
+    public int copyNum = 0;
+    public int fileDireNum = 0;
+
+    public void loginButtonClicked(){
+        System.out.println("Lets Goo");
+        
+
+
+
+
+
+    }
+
+}
