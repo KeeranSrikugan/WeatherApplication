7f818615432908582e561a245a661fc068da9e9c
Keeran Srikugan <keeran113@gmail.com>
2021-08-17 17:54:00 -0400

    checking is apiKey is hidden

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,13 +5,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Controller {
 
@@ -25,13 +32,63 @@ public class Controller {
     public int fileDireNum = 0;
 
     public void loginButtonClicked(){
-        System.out.println("Lets Goo");
-        
-
-
-
-
-
+        System.out.println("Button Clicked");
+        accessAPI();
     }
+
+
+    public void accessAPI(){
+        try {
+            apiKey WeatherAPIKey = new apiKey();
+
+
+            String apiKey = WeatherAPIKey.getAPIKey();
+            String city = "Toronto";
+
+            String sURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+            URL networkURL = new URL(sURL);
+            URLConnection conn = networkURL.openConnection();
+
+            conn.setDoOutput(false);
+            conn.setDoInput(true);
+            InputStream in = conn.getInputStream();
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+            StringBuffer buffer = new StringBuffer();
+            String line;
+
+            while ((line = input.readLine()) != null){
+                buffer.append(line);
+            }
+
+            String jsonData = buffer.toString();
+            JSONObject weatherData = new JSONObject(jsonData);
+            JSONObject tempInfo = weatherData.getJSONObject("main");
+            String currTemp = tempInfo.get("temp").toString();
+            String feelsLike = tempInfo.get("feels_like").toString();
+
+            System.out.println(currTemp);
+            System.out.println(feelsLike);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
---
+++ src/main/java/apiKey.java
@@ -0,0 +1,8 @@
+public class apiKey {
+    public String WEATHER_API_KEY = "";
+
+    public String getAPIKey(){
+        return WEATHER_API_KEY;
+    }
+
+}
